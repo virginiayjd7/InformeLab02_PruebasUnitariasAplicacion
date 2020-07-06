b963ee0120cce3433362dc8549205247cdae36b2
virginiayjd7 <37305214+virginiayjd7@users.noreply.github.com>
2020-07-06 03:36:10 -0500

    Aplicación de consola (.NET Core)

---
+++ BankTests/BankTests/BankAccountTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bank.BankAccount;
+
+namespace BankTests
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
